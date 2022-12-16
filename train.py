8caa5c56bbd15662374a3945cfca3813c9b65fea
Chanokchon <chanokchon@Chanokchons-MacBook-Pro.local>
2022-12-16 20:10:26 +0700

    Add split test mode in train script

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Python Script for Train Title Classification Model')
     parser.add_argument('-input',type=str,help='Path to Train Data in csv format',default=TRAIN_PATH)
     parser.add_argument('-pre',type=bool,help='Use Preprocess File',choices=[True,False],default=False)
+    parser.add_argument('-splittest',type=bool, default=False,help="Split Data to evaluate model" ,choices=[True,False])
     parser.add_argument('-engine',type=str,help='Choose Tokenizer Engine',choices=['deepcut','newmm','wordcut'],default='newmm')
     parser.add_argument('-model',type=str,help='Choose Model Support Vector Machine or Random Forest',choices=['svm','rf'],default='svm')
     parser.add_argument('-vectorizer',type=str,help='Chosee Vectorizer',choices=["tfidf","tf"],default="tfidf")
@@ -15,7 +16,7 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
-def cross_validate(df,save_name,engine,special_features=False,method='split',model="svm",vector="tfidf",min_count=3,save=False,list_dict=['wl1','wl2','wl3','wl4']):
+def cross_validate(df,save_name,engine,special_features=False,method='split',model="svm",vector="tfidf",min_count=3,save=False,list_dict=['wl1','wl2','wl3','wl4'],n_folds=5):
   with open(f"{REPORT_PATH}report_{save_name}.txt", "w") as text_file:
       kfold = KFold(5,shuffle=True,random_state=5)
       fold = 1
@@ -32,14 +33,13 @@ def cross_validate(df,save_name,engine,special_features=False,method='split',mod
         y_train = df_train['class'].values
         y_test = df_test['class'].values
         if vector == 'tfidf':
-          vectorizer = TfidfVectorizer(tokenizer=tokenizer,min_df=min_count)
+          vectorizer = TfidfVectorizer(tokenizer=tokenizer,min_df=min_count,token_pattern=None)
         else:
-          vectorizer = CountVectorizer(tokenizer=tokenizer,min_df=min_count)
+          vectorizer = CountVectorizer(tokenizer=tokenizer,min_df=min_count,token_pattern=None)
         sparse_matrix_train = vectorizer.fit_transform(df_train['title_th'].values)
         vector_df_train = pd.DataFrame(sparse_matrix_train.toarray(),columns=vectorizer.get_feature_names_out())
         sparse_matrix_test = vectorizer.transform(df_test['title_th'].values)
         vector_df_test = pd.DataFrame(sparse_matrix_test.toarray(),columns=vectorizer.get_feature_names_out())
-        print(f"Fold {fold} Train features : {vector_df_train.shape[1]} , Test features : {vector_df_test.shape[1]}")
         if (special_features):
           if method == "substr":
             X_train = concat_special_features_with_sub_str(df_train,vector_df_train,engine,list_dict).values
@@ -79,18 +79,28 @@ def cross_validate(df,save_name,engine,special_features=False,method='split',mod
             pickle.dump(classifier, open(f"{MODEL_PATH}{save_name}.pkl", 'wb'))
             print(f"Fold {fold} Save success Path: {MODEL_PATH}{save_name}.pkl",file=text_file)
             print(f"Fold {fold} Save success Path: {VECTOR_PATH}{save_name}.pkl",file=text_file)
+        if fold == n_folds:
+          break
         fold+=1
       pn_list = np.array(pn_list).sum(axis=0)
       print(f"All TN FP FN TP : {pn_list.ravel()}",file=text_file)
-      print(f"Cross validation recall : {np.round(re_list.mean(),4)}",file=text_file)
-      print(f"Cross validation precision : {np.round(pre_list.mean(),4)}",file=text_file)
-      print(f"Cross validation f1 : {np.round(f1_list.mean(),4)}",file=text_file)
-      print(f"Cross validation accuracy : {np.round(acc_list.mean(),4)}",file=text_file)
+      if n_folds != 1:
+        print(f"Cross validation recall : {np.round(re_list.mean(),4)}",file=text_file)
+        print(f"Cross validation precision : {np.round(pre_list.mean(),4)}",file=text_file)
+        print(f"Cross validation f1 : {np.round(f1_list.mean(),4)}",file=text_file)
+        print(f"Cross validation accuracy : {np.round(acc_list.mean(),4)}",file=text_file)
 
-def train_model(engine,pre_path=False,special_features=False,method='split',model="svm",vector="tfidf",cross=False,min_count=3,list_dict=['wl1','wl2','wl3','wl4']):
+
+def train_model(engine,pre_path=False,split_test=False,special_features=False,method='split',model="svm",vector="tfidf",cross=False,min_count=3,list_dict=['wl1','wl2','wl3','wl4']):
+      os.makedirs('./Reports', exist_ok=True)
+      os.makedirs('./Models', exist_ok=True)
+      os.makedirs('./Vectorizers', exist_ok=True)
       save_name = get_name(engine,model,vector,method,special_features,min_count,list_dict)
       print_config(engine,model,vector,method,special_features,min_count,list_dict)
-      print(f"Cross Validation : {cross}")
+      if cross:
+        print(f"Cross Validation : {cross}")
+      else:
+        print(f"Split Test : {split_test}")
       if pre_path:
         try:
           print(f"Read Preporcess PATH {PRE_PATH}{engine}.csv")
@@ -109,7 +119,9 @@ def train_model(engine,pre_path=False,special_features=False,method='split',mode
         df.to_csv(f"{PRE_PATH}{engine}.csv",index=False,encoding="utf-8")
         print(f"Save tokenizing result success")
       if cross:
-        cross_validate(df,save_name,engine,special_features,method,model,vector,min_count,False,list_dict)
+        cross_validate(df,save_name,engine,special_features,method,model,vector,min_count,False,list_dict,5)
+      elif split_test:
+        cross_validate(df,save_name,engine,special_features,method,model,vector,min_count,True,list_dict,1)
       else:
         vectors = process_vector(df['title_th'].values,save_name,min_count,vector)
         if special_features:
@@ -138,10 +150,11 @@ vector = args.vectorizer
 special = False
 method = "split"
 min_count = args.minterm
+split_test = args.splittest
 cross = args.cross
 TRAIN_PATH = args.input
 if (args.special != "None"):
   special = True
   method = args.special
 
-train_model(engine=engine,pre_path=pre,special_features=special,method=method,model=model,vector=vector,cross=cross,min_count=min_count,list_dict=list_dict)
+train_model(engine=engine,pre_path=pre,split_test=split_test,special_features=special,method=method,model=model,vector=vector,cross=cross,min_count=min_count,list_dict=list_dict)
